70025ab2284d5b467f99a1acf001c0047ebc4a36
taeik <kti1452@naver.com>
2026-06-23 20:15:35 +0900

    feat: 프론트 운영 도메인 CORS 허용

--- src/main/java/com/ceos/voting/global/security/config/SecurityConfig.java
+++ src/main/java/com/ceos/voting/global/security/config/SecurityConfig.java
@@ -79,8 +79,7 @@ public class SecurityConfig {
         // 허용 도메인
         config.setAllowedOrigins(List.of(
                 "http://localhost:3000",            // 프론트 개발 환경
-                "https://studying-developer.site",          // 운영 도메인
-                "https://www.studying-developer.site"
+                "https://next-vote-23rd-eosin.vercel.app"    // 프론트 배포 도메인
         ));
 
         // 허용 HTTP 메서드
